08cfe550b9b73026d76ff180b70e142a25c93482
Kristin198689 <163996836+Kristin198689@users.noreply.github.com>
2024-08-09 10:06:02 +0200

    fertig

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Rating from './Rating';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Выберите рейтинг:</h1>
+      <Rating />
+    </div>
+  );
+};
+
+export default App;
+
+
